ffe95e3050d511c3f8735dc96f387d4fdc1f86fe
rosesarerosieee <perezrose0211@gmail.com>
2024-08-29 13:46:55 +0800

    Added the introduction of my self and my socials

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,50 @@
+import '../App.css';
+import React, {useState, useEffect, useRef} from 'react';
+
 
 const Hero = () => {
 
+    const [animateState, setAnimateState] = useState(false);
+    const heroRef = useRef(null);
+
+    const TriggerAnimation = () => {
+        setAnimateState(true);
+    };
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const entry = entries[0];
+                if(entry.isIntersecting){
+                    TriggerAnimation();
+                } else{
+                    setAnimateState(false);
+                }
+            }, {threshold: 0.5}
+        );
+        
+        if(heroRef.current){
+            observer.observe(heroRef.current)
+        }
+        
+        return () => {
+            if(heroRef.current){
+                observer.unobserve(heroRef.current);
+            }
+        };
+        
+    },[]);
+
     return(
-    <div className="flex flex-col items-center justify-center p-5 w-full h-[100vh]">
-        <div className="relative top-0 flex flex-col items-center  bg-blue-500 text-white p-4 w-[400px] h-[80vh] rounded-[20px]">
-        <h1>Ako to si Natoy</h1>
+    <div ref={heroRef}className={`flex flex-col items-start justify-center gap-2 w-full h-[100vh] duration-1000 ease-in-out
+    ${animateState ? 'pop-up' : ''}`}>
+        <div className={`text-2xl text-red-500`}>
+            <h1>My Name is Kristian Perez</h1>
+            <h2>Born on July 2, 2003</h2>
+            <h2>I live in Marulas, Valenzuela City in Philippines</h2>
+            <h2>I do things about Web Development</h2>
+            <h2>And I Want to be become Software Engineer</h2>
         </div>
     </div>
     );
--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,21 +1,21 @@
 import React, {useState, useEffect, useRef} from "react";
-
+import '../App.css'
 
 const Intro = () => {
 
     const [animateState, setAnimateState] = useState(false);
     const introRef = useRef(null);
 
-    const animationHandle =() => {
+    const TriggerAnimation =() => {
         setAnimateState(true);
     };
 
     useEffect(() => { 
         const observer = new IntersectionObserver(
-            (enteries) => {
-                const entry = enteries[0];
+            (entries) => {
+                const entry = entries[0];
                 if(entry.isIntersecting){
-                    animationHandle();
+                    TriggerAnimation();
                 } else{
                     setAnimateState(false);
                 }
@@ -37,7 +37,7 @@ const Intro = () => {
     
     return(
         <>
-        <div ref={introRef} className={`relative flex items-center justify-center flex-col gap-10 bg-cyan w-full h-screen text-center duration-1000 ease-in-out 
+        <div ref={introRef} className={`relative flex items-center justify-center flex-col gap-10 bg-cyan w-full h-[150vh] text-center duration-1000 ease-in-out 
         ${animateState ? "pop-up": ""}`}>
             <h1 className="text-6xl text-red-500">Welcome To my Portfolio</h1>
             <h2 className="text-2xl text-red-500">In this web page, You'll learn about, My name, age, what am i, what I want to be come, my background, and socials.</h2>
