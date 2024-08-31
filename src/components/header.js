d4fe75ecf87a79caf1f95679e9dd3466bedaac28
rosesarerosieee <perezrose0211@gmail.com>
2024-08-31 21:28:10 +0800

    Added feature in navbar that it will have a background-color when you at that div

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,15 @@ import "../App.css";
 const Header = () => {
   return (
     <>
-      <header className="fixed  top-0 left-0 w-full bg-cyan-500 shadow z-[999999999]">
+      <header className="fixed top-0 left-0 w-full bg-cyan-500 shadow z-[999999999]">
         <ul className="flex items-center justify-end gap-[20px] h-[12vh] pr-5 text-[1.3rem]">
           <li className="link mr-4">
             <Link
               to="intro"
               smooth={true}
               duration={500}
+              spy={true}
+              activeClass="active-link"
               className="cursor-pointer transition-all duration-1000 ease-in-out hover:text-[3rem] hover:text-red-500"
             >
               Intro
@@ -22,6 +24,8 @@ const Header = () => {
               to="hero"
               smooth={true}
               duration={500}
+              spy={true}
+              activeClass="active-link"
               className="cursor-pointer transition-all duration-1000 ease-in-out hover:text-[3rem] hover:text-red-500"
             >
               Main Section
@@ -30,26 +34,30 @@ const Header = () => {
 
           <li className="link mr-4">
             <Link
-              to="socials"
-              smooth={true}
-              duration={500}
-              className="cursor-pointer transition-all duration-1000 ease-in-out hover:text-[3rem] hover:text-red-500"
-            >
-              Socials
-            </Link>
-          </li>
-
-          <li className="link mr-4">
-            <Link
               to="mybackground"
               smooth={true}
               duration={500}
+              spy={true}
+              activeClass="active-link"
               className="cursor-pointer transition-all duration-1000 ease-in-out hover:text-[3rem] hover:text-red-500"
             >
               Background
             </Link>
           </li>
 
+          <li className="link mr-4">
+            <Link
+              to="socials"
+              smooth={true}
+              duration={500}
+              spy={true}
+              activeClass="active-link"
+              className="cursor-pointer transition-all duration-1000 ease-in-out hover:text-[3rem] hover:text-red-500"
+            >
+              Socials
+            </Link>
+          </li>
+          
         </ul>
       </header>
     </>
