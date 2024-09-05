d2418cf33b63f55c5ce58c9c27cf8c3461e820cd
rosesarerosieee <perezrose0211@gmail.com>
2024-09-05 12:29:35 +0800

    Added the Iphone Container, for the mobile view of my projects

--- src/components/myproject.js
+++ src/components/myproject.js
@@ -1,30 +1,86 @@
 const MyProjects = () => {
   return (
     <div
-      className={`grid grid-cols-[repeat(3,300px)] grid-rows-[repeat(3,full)] relative w-full h-[200vh] items-center justify-center`}
+      className={`grid grid-cols-[repeat(3,450px)] grid-rows-[repeat(3,full)] relative w-full h-[200vh] items-start justify-center`}
     >
-      <div className="flex items-center justify-center">
-        <div className="w-[150px] h-[50vh] bg-red-500">
-          <h1>Project1</h1>
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
         </div>
       </div>
-      <div className="">
-        <h1>Project 2</h1>
-      </div>
-      <div className="">
-        <h1>Project 3</h1>
-      </div>
-      <div className="">
-        <h1>Project 4</h1>
-      </div>
-      <div className="">
-        <h1>Project 5</h1>
-      </div>
-      <div className="">
-        <h1>Project 6</h1>
-      </div>
-   
 
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-[20px] flex items-center justify-center">
+        {/* iPhone Container */}
+        <div className="relative w-[250px] h-[450px] bg-white rounded-[40px] overflow-hidden border-[14px] border-gray-700 shadow-lg">
+          {/* iPhone Notch */}
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-b-[20px]"></div>
+
+          {/* Screen Content */}
+          <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-black">Tite kaba boss?</h1>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 };
